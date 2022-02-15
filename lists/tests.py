8ba8a8abf97ce2fd4838ed7f6927a54c81a533e1
postog78 <postog@mail.ru>
2022-02-15 11:54:24 +0300

    Странное изменение. Чистка, но одна ошибка неверная, не так что по книжке

--- lists/tests.py
+++ lists/tests.py
@@ -14,28 +14,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
     
-    def test_can_save_a_POST_request(self):
-        '''тест: можно сохранить post-запрос'''
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-    
-    def test_redirects_after_POST(self):
-        '''тест: переадресует после post-запроса'''
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        
-        self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response['location'], '/')
-        self.assertEqual(response['location'], '/lists/one_list/')
-    
-    def test_only_saves_items_when_necessary(self):
-        "тест: сохраняет элементы, только когда это нужно"
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-        
-        
+      
 class ItemModelTest(TestCase):
     '''тест модели элемента списка'''
     
@@ -75,3 +54,19 @@ class ListViewTest(TestCase):
         
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        
+class NewListTest(TestCase):
+    '''тест нового списка'''
+    
+    def test_can_save_a_POST_request(self):
+        '''тест: можно сохранить post-запрос'''
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_redirects_after_POST(self):
+        '''тест: переадресует после post-запроса'''
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/one_list/')
+        
--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,14 @@ from lists.models import Item
 
 def home_page(request):
     '''домашнаяя страница'''
-    if request.method == 'POST':
-        Item.objects.create(text = request.POST['item_text'])
-        return redirect('/lists/one_list/')
-
     return render(request, 'home.html')
 
 def view_list(request):
     '''представление списка'''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    '''новый список'''
+    Item.objects.create(text = request.POST['item_text'])
+    return redirect('/lists/one_list/')
