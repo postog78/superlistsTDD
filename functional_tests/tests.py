481d4f68111d999eaa5e6f28c7d146effb49ffb2
postog78 <postog@mail.ru>
2022-02-16 17:41:29 +0300

    Первые шаги ФТ для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -121,8 +121,31 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertNotIn('Сделать мушку', page_text)
-        
+   
+  
+    def test_layout_and_styling(self):
+       '''тест макета и стилевого оформления'''
+       #Эдит открывает домашнюю страницу
+       self.browser.get(self.live_server_url)
+       self.browser.set_window_size(1024, 768)
        
+       #Она замечает, что поле ввода аккуратно центрированно
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       self.assertAlmostEqual(
+           inputbox.location['x'] + inputbox.size['width'] / 2,
+           512,
+           delta=10
+           )
+       #Она начинает новый список и видит, что поле ввода там тоже аккуратно центрированно
+       inputbox.send_keys("testing")
+       inputbox.send_keys(Keys.ENTER)
+       self.wait_for_row_in_list_table("1: testing")
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       self.assertAlmostEqual(
+           inputbox.location['x'] + inputbox.size['width'] / 2,
+           512,
+           delta=10
+           )
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
